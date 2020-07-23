41baef73a84ee55066797e5d1723dc9411ccea36
davidruvolo51 <davidruvolo51@gmail.com>
2020-07-23 20:11:38 +0200

    completed accordion component

--- R/accordion.R
+++ R/accordion.R
@@ -9,16 +9,16 @@
 #'
 #' @param inputId a unique ID for the accordion component
 #' @param title a text string containing a title for the collapsible section
-#' @param html an html element or a list of html elements
+#' @param content an html element or a list of html elements
 #' @param heading_level adjust the HTML heading level; default is "h3". Use
 #'      on of the following headings: h1, h2, h3, h4, h5, h6
 #' @param class a string containing css classes. Using this argument, you can
 #'      pass your own class names or use one of the classes made available by
 #'      this package: "accordion__style__a" (styling used in the app). Use
-#'      `class = NULL`, to return a minimally styled component.
+#'      "class = NULL", to return a minimally styled component.
 #'
-#' **Notes on `heading_level`** By default, the title is rendered into
-#' a <h3> element. This element may not always work in all
+#' Notes on `heading_level`: By default, the title is rendered into
+#' a h3 element. This element may not always work in all
 #' situations as it is difficult to determine the markup and
 #' context the accordion element is used. This option will allow
 #' you to use the accordion element with the document's hierarchy
@@ -27,9 +27,8 @@
 #' @examples
 #' if (interactive()) {
 #'   library(shiny)
-#'   library(accessibleshiny)
 #'   ui <- tagList(
-#'     accessibleshiny::use_accessibleshiny(),
+#'     iceComponents::use_iceComponents(),
 #'     tags$head(
 #'       tags$style(
 #'         "#what-is-shiny {
@@ -41,11 +40,11 @@
 #'       id = "main",
 #'       class = "main",
 #'       tags$h2("Frequently Asked Questions Page"),
-#'       tags$p("Here are some commonly asked questions")
-#'       accordion(
+#'       tags$p("Here are some commonly asked questions"),
+#'       iceComponents::accordion(
 #'         inputId = "what-is-shiny",
 #'         title = "What is Shiny?",
-#'         html = tagList(
+#'         content = tagList(
 #'           tags$p(
 #'             "Shiny is an R package that makes it easy to build",
 #'             "interactive web apps straight from R. You can host",
@@ -62,13 +61,13 @@
 #'   server <- function(input, output) {}
 #'   shinyApp(ui, server)
 #' }
-#' @keywords accessibleshiny accordion collapsible content
+#' @keywords iceComponents accordion collapsible content
 #' @return Create an accordion component
 #' @export
 accordion <- function(
     inputId,
     title,
-    html,
+    content,
     heading_level = "h3",
     class = "accordion__style__a"
 ) {
@@ -76,7 +75,7 @@ accordion <- function(
     # validate
     if (!is.character(inputId)) stop("argument 'inputId' must be a string")
     if (!is.character(title)) stop("argument 'title' must be a string")
-    if (is.null(html)) stop("argument 'html' cannot be null")
+    if (is.null(content)) stop("argument 'content' cannot be null")
 
     # validate html headings
     valid_html_headings <- c("h1", "h2", "h3", "h4", "h5", "h6")
@@ -103,7 +102,7 @@ accordion <- function(
         ),
         accordion_helpers$content(
             ids = ids,
-            html = html
+            content = content
         )
     )
 
@@ -127,10 +126,9 @@ accordion <- function(
 #' @param inputId the of of the component to reset
 #'
 #' @keywords iceComponents accordion reset
-#' @importFrom shiny getDefaultReactiveDomain
 #' @export
 reset_accordion <- function(inputId) {
-    session <- getDefaultReactiveDomain()
+    session <- shiny::getDefaultReactiveDomain()
     session$sendInputMessage(
         inputId = inputId,
         message = "reset"
