4cf5b4fe7e6182e550e2b8f948722176237ba506
davidruvolo51 <davidruvolo51@gmail.com>
2020-10-12 20:34:18 +0200

    added navigation component

---
+++ R/navigation.R
@@ -0,0 +1,106 @@
+#' Page Navigation
+#'
+#' Create a container for page navigation buttons. The `navigation` function
+#' returns a container for the page navigation buttons.
+#'
+#' @param ... Navigation buttons (`back_btn`, `forward_btn`)
+#'
+#' @examples
+#' if (interactive()) {
+#'   library(shiny)
+#'   library(iceComponents)
+#'
+#'   # define app
+#'   ui <- tagList(
+#'     use_iceComponents(),
+#'     container(
+#'       tags$h2("Count by 5's"),
+#'       uiOutput("count"),
+#'       navigation(
+#'         back_btn(inputId = "subtract", label = "Subtract 5"),
+#'         back_btn(inputId = "add", label = "Add 5"),
+#'       )
+#'     )
+#'   )
+#'
+#'   # dynamically render pages based on page navigation
+#'   server <- function(input, output) {
+#'
+#'     counter <- reactiveVal(1)
+#'
+#'     observeEvent(input$subtract, {
+#'       counter(counter() - 5)
+#'       output$counter <- renderUI(tags$p(counter()))
+#'     })
+#'
+#'     observeEvent(input$add, {
+#'       counter(counter() + 5)
+#'       output$count <- renderUI(tags$p(counter()))
+#'     })
+#'
+#'   }
+#'
+#'   shinyApp(ui, server)
+#' }
+#'
+#' @export
+navigation <- function(...) {
+    elems <- tagList(...)
+    u <- tags$ul()
+    sapply(seq_len(length(elems)), function(x) {
+        u$children[[x]] <<- tags$li(class = "nav__list__item", elems[[x]])
+    })
+
+    if (length(elems) == 1) u$attribs$class <- "nav__list single__list"
+    if (length(elems) > 1) u$attribs$class <- "nav__list multi__list"
+
+    tags$div(class = "nav__container", u)
+}
+
+
+#' Back Button
+#'
+#' Create a back button for use in the navigation container. By default,
+#' this button renders with the inputId defined as `backBtn`. The button can
+#' be accessed in the shiny server via `input$backBtn`. You can also set the
+#' button label. The default is `Previous`.
+#'
+#' @param inputId a unique Id for the button (default `backBtn`)
+#' @param label customize the button (default `Previous`)
+#'
+#' @noRd
+back_btn <- function(inputId = "backBtn", label = "Previous") {
+    tags$button(
+        id = inputId,
+        class = "shiny-bound-input action-button ice__btn btn__default",
+        rheroicons::rheroicon(
+            name = "chevron_left",
+            type = "outline"
+        ),
+        label
+    )
+}
+
+
+#' Forward Button
+#'
+#' Create a forward button for use in the navigation container. By default,
+#' this button renders with the inputId defined as `forwardBtn`. The button can
+#' be accessed in the shiny server via `input$forwardBtn`. You can also set the
+#' button label. The default is `Next`.
+#'
+#' @param inputId a unique Id for the button (default `forwardBtn`)
+#' @param label set the button label (default `Next`)
+#'
+#' @noRd
+forward_btn <- function(inputId = "forwardBtn", label = "Next") {
+    tags$button(
+        id = inputId,
+        class = "shiny-bound-input action-button ice__btn btn__primary",
+        label,
+        rheroicons::rheroicon(
+            name = "chevron_right",
+            type = "outline"
+        )
+    )
+}
